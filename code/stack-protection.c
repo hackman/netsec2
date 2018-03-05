d426afbaa00c87b97aa084d67e395fd5a1f98417
Marian Marinov <mm@yuhu.biz>
2018-03-06 00:25:53 +0200

    code/stack-protection.c: Introduced fun() to show the error messages with alloca() - also using fun() to show how you can control the compilation with -fstack-protector-explicit and attributes
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- code/stack-protection.c
+++ code/stack-protection.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <alloca.h>
 // this disables "warning: implicit declaration of function 'gets'"
 #pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
+
+//void __attribute__ ((stack_protect)) fun() {
+void fun() {
+	char *buf = alloca(0x100);
+	/* Don't allow gcc to optimise away the buf */
+	asm volatile("" :: "m" (buf));
+}
 
 void user_read() {
         char buffer[28];
