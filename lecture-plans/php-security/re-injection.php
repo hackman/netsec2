085388562ae0a17bb413fc06de2b7e989d6882c9
Marian Marinov <mm@yuhu.biz>
2018-03-22 14:27:08 +0200

    lecture-plans/php-security/re-injection.php: Added two new lines to make the output more visible
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- lecture-plans/php-security/re-injection.php
+++ lecture-plans/php-security/re-injection.php
@@ -5,7 +5,7 @@
 $username = (isset($_GET['username'])) ? $_GET['username'] : '';
 $password = (isset($_GET['password'])) ? $_GET['password'] : '';
 $newpassword = (isset($_GET['newpassword'])) ? $_GET['newpassword'] : '';
-echo("New pass: $newpassword<br />");
+echo("New pass: $newpassword<br />\n\n");
 $users = file_get_contents('users');
 $users = preg_replace("/($username):$password(:.*)/e", "'\\1:'.strtolower('$newpassword').'\\2'", $users );
 $users = preg_replace("#[\r\n]+#", "<br />\n", $users, -1 );
